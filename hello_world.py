63fb5a36d903e31ce42af579ba4c703f985c3648
Eugen Wiens <eugen.wiens@jumo.net>
2025-03-06 20:28:02 +0100

    test

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
 print("Hello, World!")
+
+class NumberProcessor:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+        
+# Beispielverwendung
+processor = NumberProcessor(10, 5)
+print("Addition:", processor.add())
+print("Subtraktion:", processor.subtract())
+print("Multiplikation:", processor.multiply())
+print("Division:", processor.divide())
